04d24e5fd91b483d8317bbc049e7ff86c6de689a
kerembasol <kerembasol@3e91fdc6-a154-11dd-988f-d3ef9af6d102>
2008-11-12 08:25:06 +0000

    Saving is done via servlet instead of jsp

---
+++ trunk/pennfitness/WEB-INF/src/model/RouteSaveServlet.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import entities.Route;
+
+
+
+public class RouteSaveServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {    
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {      
+    	
+    	PrintWriter out = resp.getWriter();
+    	
+    	String routeName  = req.getParameter("routeName");
+    	String routeDist  = req.getParameter("distance");
+    	String routeDesc  = req.getParameter("routeDesc");
+    	String routeColor = req.getParameter("routeColor");
+    	String routePts   = req.getParameter("pvalue");
+    	
+    	
+    	//TODO
+    	//HttpSession
+    	//creatorId will be read from cookies. 
+    	Route route = new Route(routeName, routeColor, routePts, Float.valueOf(routeDist), routeDesc, 1/*creatorId*/ );
+    	   	
+        out.println( DBUtilRoute.saveRoute( route ) );
+        
+    }
+}
