f0ca74610ef54479dd1f9061c4923855c10575ac
kerembasol <kerembasol@3e91fdc6-a154-11dd-988f-d3ef9af6d102>
2008-11-13 04:47:49 +0000

    DB utility functions for Event (save/modify/getAll)

---
+++ trunk/pennfitness/WEB-INF/src/model/DBUtilEvent.java
@@ -0,0 +1,154 @@
+package model;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Event;
+import entities.Route;
+
+public class DBUtilEvent {
+
+	
+	/**
+	 * Saves a new event with the given information
+	 * 
+	 * @param event
+	 * @return the ID of newly inserted event if transaction is successful. -1 if not.
+	 */
+	public static int saveEvent( Event event ){
+					
+		String saveQuery = 
+			"INSERT INTO Event ( name, description, groupID, publicity, creatorID, createdDate, modifiedDate, " +
+								"routeID, eventTypeID, eventDate, duration, eventTime, publicity  )"+
+							   " VALUES ('"+ event.getName()+"'," +
+										"'"+ event.getDescription()+"'," +
+										"'"+ event.getGroupID()+"'," +
+										"'"+ event.getPublicity()+"'," +
+										"'"+ event.getCreatorID()+"'," +
+										"'"+ new Date( System.currentTimeMillis() ) +"'," +
+										"'"+ new Date( System.currentTimeMillis() ) +"'," +
+										"'"+ event.getRouteID() +"'," +
+										"'"+ event.getEventTypeID() +"'," +
+										"'"+ event.getEventDate() +"'," +
+										"'"+ event.getDuration() +"'," +
+										"'"+ event.getPublicity() +"'," +
+										"'"+ event.getEventTime() +"')"; 
+												
+		if( DBConnector.executeUpdateQuery( saveQuery ) > 0 ){
+			
+			try {
+				ResultSet  result = DBConnector.getQueryResult( "SELECT MAX( eventID ) FROM Event" );
+				if( result != null )
+					if( result.next() )
+						return result.getInt( 1 );
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				System.out.println("DBUtilEvent.saveEvent() : Error getting ID of saved event"  );
+				e.printStackTrace();
+			}
+			
+		}
+			
+		return -1;
+	}
+	
+	
+	/**
+	 * Updates the given event with the given ID.
+	 * 
+	 * @param event
+	 * @return 1 if successful, -1 otherwise.
+	 */
+	public static int modifyEvent( Event event ){
+		
+		String updateQuery = 
+			"UPDATE Event " +
+			"SET eventName='" + event.getName() + "'," +
+			    "duration='" + event.getDuration() + "'," +
+			    "description='" + event.getDescription() + "'," +
+			    "routeID='" + event.getRouteID() + "'," +
+			    "groupID='" + event.getGroupID() + "'," +
+			    "creatorID='" + event.getCreatorID() + "'," +
+			    "eventTime='" + event.getEventTime() + "'," +
+			    "eventDate='" + event.getEventDate() + "'," +
+			    "publicity='" + event.getPublicity() + "' " +
+			"WHERE eventID='" + event.getEventID() + "'";
+		
+		return DBConnector.executeUpdateQuery( updateQuery );
+		
+	}
+	
+	/**
+	 * Function that gets all the events registered so far
+	 * 
+	 * @return
+	 */
+	public static List<Event> getAllEvents( ){
+		
+		List<Event> events = new ArrayList<Event>();
+		ResultSet resultSet = DBConnector.getQueryResult( "SELECT * FROM Event" );	
+		
+		try {
+			while( resultSet.next() ){				
+				events.add( resultSetToEvent( resultSet ) );
+			}
+		} 
+		catch (SQLException e) {
+			System.out.println("DBUtilEvent.getAllEvents() : Error getting all events");
+			e.printStackTrace();
+		}
+		finally{
+			DBConnector.closeDBConnection();
+		}
+			
+		return events;
+	}
+	
+	
+	/**
+	 * Utility function that gets Event object from a resultset row
+	 * 
+	 * @param resultSet
+	 * @return
+	 */
+	private static Event resultSetToEvent( ResultSet resultSet ){
+		
+		
+		int eventID, groupID, creatorID, routeID, eventTypeID;
+		float duration;
+		char publicity;
+		String name, description, eventTime;
+		Date createdDate, modifiedDate, eventDate; 	 	 	 	 	 	 	 	
+		
+		try {
+			eventID = resultSet.getInt("eventID");
+			name = resultSet.getString("name");
+			description = resultSet.getString("description");
+			groupID = resultSet.getInt("groupID");
+			publicity = resultSet.getString("publicity").charAt(0);
+			creatorID = resultSet.getInt("creatorID");
+			createdDate = resultSet.getDate("createdDate");
+			modifiedDate = resultSet.getDate("modifiedDate");
+			routeID = resultSet.getInt("routeID");
+			eventTypeID = resultSet.getInt("eventTypeID");
+			eventDate = resultSet.getDate("eventDate");
+			eventTime = resultSet.getString("eventTime");
+			duration = resultSet.getFloat("duration");
+			
+			
+			return new Event( eventID, name, description, groupID, publicity, creatorID, 
+					  		  createdDate, modifiedDate,  routeID, eventTypeID, eventDate, eventTime, duration);
+			
+		} 
+		catch (SQLException e) {
+			System.out.println("DBFunctionUtil.resultSetToEvent() : Error converting resultset to event");
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+}
