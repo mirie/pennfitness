2888a015e4585c3db852eb70338f9030a4545de9
kerembasol <kerembasol@3e91fdc6-a154-11dd-988f-d3ef9af6d102>
2008-11-22 04:44:30 +0000

    Servlet for searching events

---
+++ trunk/pennfitness/WEB-INF/src/model/EventSearchServlet.java
@@ -0,0 +1,89 @@
+package model;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+
+import entities.Event;
+
+public class EventSearchServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {    
+		doPost(req, resp);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {      
+    	JSONObject result = new JSONObject();
+    	PrintWriter out = resp.getWriter();
+    	
+    	String keyword  = req.getParameter("keyword");    	
+    	String type     = req.getParameter("type");
+    	String fromDate = req.getParameter("fromDate");
+    	String toDate   = req.getParameter("toDate");
+    	
+    	
+    	Map<String, String> params = new HashMap<String, String>();
+    	params.put( " name", " LIKE '%"+keyword+"%'" );
+    	params.put( " description", " LIKE '%"+keyword+"%'" );
+    	params.put( " groupID", " = '"+type+"'" );
+    	params.put( " eventDate", " > '" +fromDate+ "'" );
+    	params.put( " eventDate", " < '" +toDate+ "'" );
+
+    	
+    	List<Event> events = DBUtilEvent.searchForEvents( params );
+    	  	
+    	if( events != null ){
+    	
+	    	Iterator<Event> iterator = events.iterator();
+	    	
+	    	StringBuffer sbuf = new StringBuffer();
+	    	sbuf.append("<div class = \"Result\">\n").
+	    		 append("\t<div class=\"EventInfoList\">\n");
+	    		 
+	    	Event event;
+	    	while( iterator.hasNext() ){
+	    		event = iterator.next();
+	    		sbuf.append("\t\t<div class=\"registeredEventItem\">\n").
+	    			 append("\t\t\t<a href=\"function_to_show_event\">"+ event.getName() +"</a>\n").
+	    			 append("</div>\n");
+	    	}
+	    	
+	    	sbuf.append("\t</div>\n").
+	    		 append("</div>\n");
+	    	
+	    	
+	    	result.put( "STATUS", "Success");
+	    	result.put( "DATA", sbuf.toString() );
+	    	
+	    	out.println( sbuf.toString() );
+	    	
+    	}
+    	else{
+    		result.put( "STATUS", "Failure");
+    		//TODO
+    		//I'll put more detailed MSG here
+    		result.put( "MSG", "Error in DB" );
+    	}
+    	
+    	out.println( result );
+    	       
+    }	
+	
+}
