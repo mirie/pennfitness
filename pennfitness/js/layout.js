0f43915e979efe5c5efc1866cf865d43a941e7a7
inseob.lee <inseob.lee@3e91fdc6-a154-11dd-988f-d3ef9af6d102>
2008-11-24 18:25:54 +0000

    Corrected header height

--- pennfitness/js/layout.js
+++ pennfitness/js/layout.js
@@ -2,7 +2,7 @@
 function createBaseLayout() {
         var layout = new YAHOO.widget.Layout({
             units: [
-                { position: 'top', height: 72, body: 'header', gutter: '0px', resize: false },    
+                { position: 'top', height: 105, body: 'header', gutter: '0px', resize: false },    
                 { position: 'bottom', height: 20, body: 'archFooter', gutter: '0px', resize: false },
                 { position: 'left', header:'Penn Fitness', width: 200, resize: false, body: 'leftMenu', gutter: '0px', collapse: true },
                 { position: 'center', minWidth: 400, minHeight: 200, body: 'centerArea', gutter: '1px 0px 0px 1px' }
