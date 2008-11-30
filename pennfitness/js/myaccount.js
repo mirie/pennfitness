842ab8a77ca4daaf08ecfcf264ece0d9f93808ff
kerembasol <kerembasol@3e91fdc6-a154-11dd-988f-d3ef9af6d102>
2008-11-30 05:35:39 +0000

    group integration...

--- pennfitness/js/myaccount.js
+++ pennfitness/js/myaccount.js
@@ -54,3 +54,102 @@ function saveGp() {
 	var transaction = YAHOO.util.Connect.asyncRequest("POST", "saveGroup.do", callback, strData);
 }
 
+function populateGroupByUserID()
+{		
+
+	var successHandler = function(o) {
+		var response;
+		if( (jResponse = parseNCheckByJSON(o.responseText)) == null ) return false;
+	
+		var groupSelect = document.getElementById("myGroupList");
+		while(groupSelect.hasChildNodes() == true){
+			groupSelect.removeChild(groupSelect.childNodes[0]);
+		}
+		
+		
+		var groupList = jResponse.DATA.GROUPS.split(";");
+		groupSelect.length = 1;
+		
+		for (var i = 0; i < groupList.length - 1; i++) {
+			var group = groupList[i].split("-");	
+			
+			/*var checkbox = document.createElement('input');
+			checkbox.type = 'checkbox';
+			checkbox.name = 'checkboxName';
+			checkbox.defaultChecked = false;
+			checkbox.value = 'Kibology';
+			checkbox.onclick =  function(){ alert('kerem') };*/
+			
+			var link = document.createElement('a');
+			link.setAttribute('href','javascript:GetGroupDetail(\'' +group[0]+ '\')');
+			link.appendChild(document.createTextNode(group[1]));
+			link.appendChild(document.createElement('br'));
+			
+			groupSelect.appendChild(link);
+		}
+	}
+	
+	var failureHandler = function(o) {
+		alert("Error + " + o.status + " : " + o.statusText);
+	}
+	
+	var callback = {
+		success:successHandler,
+		failure:failureHandler,
+	}
+	
+	var strData = "action=getGroups";
+	
+	var transaction = YAHOO.util.Connect.asyncRequest("POST", "mgGroupReg.do", callback, strData);
+}
+
+function GetGroupDetail( groupID )
+{
+		var successHandler = function(o) {
+		var response;
+		if( (jResponse = parseNCheckByJSON(o.responseText)) == null ) return false;
+	
+		var groupDetail = document.getElementById("myGroupDetail");
+		while(groupDetail.hasChildNodes() == true){
+			groupDetail.removeChild( groupDetail.childNodes[0] );
+		}
+		
+		groupDetail.appendChild( document.createTextNode(jResponse.DATA.NAME) );
+		groupDetail.appendChild( document.createTextNode(jResponse.DATA.CREATE_DATE) );
+		groupDetail.appendChild( document.createTextNode(jResponse.DATA.CREATED_BY) );
+		groupDetail.appendChild( document.createTextNode(jResponse.DATA.DESCRIPTION) );
+		groupDetail.appendChild( document.createTextNode(jResponse.DATA.MEMBER_COUNT) );
+
+	}
+	
+	var failureHandler = function(o) {
+		alert("Error + " + o.status + " : " + o.statusText);
+	}
+	
+	var callback = {
+		success:successHandler,
+		failure:failureHandler,
+	}
+	
+	var strData = "action=getDetailGroupInfo&groupID="+groupID;
+	
+	var transaction = YAHOO.util.Connect.asyncRequest("POST", "mgGroup.do", callback, strData);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
