9900e14a8d27e23a3ba1268580f2f17cdeb8f21f
qianliuster <qianliuster@3e91fdc6-a154-11dd-988f-d3ef9af6d102>
2008-12-04 02:59:30 +0000

    Added modifyPersonalInfo, Cancel, save functions for the buttons in myAccount overlay

--- trunk/pennfitness/js/myaccount.js
+++ trunk/pennfitness/js/myaccount.js
@@ -53,10 +53,8 @@ function saveGp() {
 	
 	var transaction = YAHOO.util.Connect.asyncRequest("POST", "saveGroup.do", callback, strData);
 }
-
 function populateGroupByUserID()
 {		
-
 	var successHandler = function(o) {
 		var response;
 		if( (jResponse = parseNCheckByJSON(o.responseText)) == null ) return false;
@@ -136,20 +134,89 @@ function GetGroupDetail( groupID )
 	var transaction = YAHOO.util.Connect.asyncRequest("POST", "mgGroup.do", callback, strData);
 
 }
+function modifyPersonalInfo() {	
 
+	document.getElementById("userGenderTxt").style.display = "block";
+	document.getElementById("userEmailTxt").style.display = "block";
+	document.getElementById("userPasswordTxt").style.display = "block";
+	
+	document.getElementById("userGender").style.display = "none";
+	document.getElementById("userEmail").style.display = "none";
+	document.getElementById("userPassword").style.display = "none";
+	
+	document.getElementById("savePersonalInfo").style.display = "block";
+	document.getElementById("modifyPersonalInfo").style.display = "none";
 
+}
+function cancelPersonalInfo() {	
 
+	document.getElementById("userGenderTxt").style.display = "none";
+	document.getElementById("userEmailTxt").style.display = "none";
+	document.getElementById("userPasswordTxt").style.display = "none";
+	
+	document.getElementById("userName").style.display = "block";
+	document.getElementById("userGender").style.display = "block";
+	document.getElementById("userEmail").style.display = "block";
+	document.getElementById("userPassword").style.display = "block";
+	
+	document.getElementById("savePersonalInfo").style.display = "none";
+	document.getElementById("modifyPersonalInfo").style.display = "block";
 
+}
+// ***********************************************************************
+// Function: savePersonalInfo
+// Saves the modified personal information. 
+// Displays alert if missing user name. 
+// ***********************************************************************
+function savePersonalInfo() {
+			
+	// FOR JSON Handling
+	var successHandler = function(o) {	
+		var response;
+		
+	    // Use the JSON Utility to parse the data returned from the server
+	    try {
+	       response = YAHOO.lang.JSON.parse(o.responseText); 
+	    }
+	    catch (x) {
+	        alert("JSON Parse failed!");
+	        return;
+	    }        
+	
+	    if (response.STATUS == 'Success') { // Modified UserID passed back
+			alert("User Information modified successfully");
+			
+			//YAHOO.leftTB.group.getNewGroupNames();
+		}
+		else {
+			alert("Personal Information was not saved!");
+		}
+	}
 
+	var failureHandler = function(o) {
+		alert("Error + " + o.status + " : " + o.statusText);
+	}
 
+	var callback = {
+		failure:failureHandler,
+		success:successHandler,
+		timeout:3000,
+	}
+	
+	// POST string data
+	var strData = "";
+	strData += "gender=" + document.getElementById("userGenderTxt").value + "&";
+	strData += "email=" + document.getElementById("userEmailTxt").value + "&";
+	strData += "password=" + document.getElementById("userPasswordTxt").value + "&";
+	
+	// Append GroupID to strData
+	//strData += "userID=" + document.getElementById("user.getUserID()").value.trim();
+	
+	var transaction = YAHOO.util.Connect.asyncRequest("POST", "modifyUser.do", callback, strData);
+}
 
-
-
-
-
-
-
-
-
-
-
+//Listeners
+YAHOO.util.Event.addListener("modifyPersonalInfoBtn", "click", modifyPersonalInfo);	
+YAHOO.util.Event.addListener("cancelPersonalInfoBtn", "click", cancelPersonalInfo);
+YAHOO.util.Event.addListener("savePersonalInfoBtn", "click", savePersonalInfo);
+=======
