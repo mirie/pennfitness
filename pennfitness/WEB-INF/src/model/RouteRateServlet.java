dd9e5c74866574e12bcab00550187e28c565ae4a
inseob.lee <inseob.lee@3e91fdc6-a154-11dd-988f-d3ef9af6d102>
2008-12-04 08:41:17 +0000

    Added 'rate a route'

---
+++ pennfitness/WEB-INF/src/model/RouteRateServlet.java
@@ -0,0 +1,107 @@
+package model;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+import entities.User;
+import org.json.simple.JSONObject;
+
+public class RouteRateServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {    
+		doPost(req, resp);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException
+    {      
+    	PrintWriter out = resp.getWriter();
+		HttpSession session = req.getSession();
+		JSONObject result = new JSONObject();
+
+    	String sceneryStr	= req.getParameter("scenery");    	
+    	String diffStr 		= req.getParameter("difficulty");
+    	String safetyStr	= req.getParameter("safety");
+    	String rateStr		= req.getParameter("rate");
+    	String routeID		= req.getParameter("routeID");
+    	User user			= (User)session.getAttribute("user");
+    	String userID		= "";
+
+    	int scenery, difficulty, safety, rate;
+    	
+    	if( user == null )
+    	{
+    		result.put("STATUS", "Failure");
+    		result.put("MSG", "Please login to rate a route");
+        	out.print(result);
+        	return;
+    	}
+    	
+    	try
+    	{
+        	userID		= user.getUserID();
+    		scenery		= Integer.parseInt(sceneryStr);
+    		difficulty	= Integer.parseInt(diffStr);
+    		safety		= Integer.parseInt(safetyStr);
+    		rate		= Integer.parseInt(rateStr);
+    		
+    		if(routeID == null || routeID.length() == 0) throw new Exception();
+    	}
+    	catch(Exception ex)
+    	{
+    		ex.printStackTrace();
+    		result.put("STATUS", "Failure");
+    		result.put("MSG", "Invalid argument");
+        	out.print(result);
+        	return;
+    	}
+    	
+    	if( DBUtilRoute.checkIfRatedRoute(routeID, userID) )
+    	{
+    		result.put("STATUS", "Failure");
+    		result.put("MSG", "You can rate a route only once!");
+        	out.print(result);
+        	return;
+    	}
+    	
+    	List<Float> rates = DBUtilRoute.rateRoute(routeID, userID, scenery, difficulty, safety, rate);
+    	
+    	if(rates == null)
+    	{
+    		result.put("STATUS", "Failure");
+    		result.put("MSG", "Error while updating ratings");
+        	out.print(result);
+        	return;
+    	}
+    	
+    	String rating = "";
+    	for(int i = 0 ; i < rates.size() ; i++)
+    	{
+    		rating += rates.get(i) + (i != rates.size() - 1 ? ";" : "");
+    	}
+    	
+		result.put("STATUS", "Success");
+		result.put("DATA", rating);
+    	out.print(result);
+    	
+    }
+
+
+}
+
+
+
+
+
+
